ccbbf03f581212379c33a9f5fa315b96898746e8
Salman Zafar <salman.zafar@innowi.com>
2019-03-19 18:06:16 +0500

    Desging project list and detail templates

--- budget/urls.py
+++ budget/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpattern = [
+urlpatterns = [
 
+    path('', views.project_list, name="project.list"),
+    path('<slug:project_slug>', views.project_detail, name="project.detail"),
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def project_list(request):
+    return render(request, 'budget/project_list.html', {})
+
+
+def project_detail(request, project_slug):
+    ## fetching the correct project
+    return render(request, 'budget/project_detail.html', {})
