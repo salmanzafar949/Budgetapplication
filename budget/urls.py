5b02ff26aa95c811a4f77d725f226c6ba48326fa
Salman Zafar <salman.zafar@innowi.com>
2019-03-20 11:59:11 +0500

    Add new project implmeneted

--- budget/urls.py
+++ budget/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 
     path('', views.project_list, name="project.list"),
+    path('add', views.ProjectCreateView.as_view(), name="project.add"),
     path('<slug:project_slug>', views.project_detail, name="project.detail"),
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Project
-
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from .models import Project, Category
+from django.views.generic import CreateView
+from django.utils.text import slugify
 # Create your views here.
 
 def project_list(request):
@@ -11,3 +12,25 @@ def project_detail(request, project_slug):
     ## fetching the correct project
     project = get_object_or_404(Project, slug=project_slug)
     return render(request, 'budget/project_detail.html', {'project': project})
+
+class ProjectCreateView(CreateView):
+    model = Project
+    template_name = 'budget/project_add.html'
+    fields = ['name', 'budget']
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.save()
+
+        categories = self.request.POST['categoriesString'].split(',')
+
+        for category in categories:
+            Category.objects.create(
+                project=Project.objects.get(id=self.object.id),
+                name = category
+            ).save()
+
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return slugify(self.request.POST['name'])
